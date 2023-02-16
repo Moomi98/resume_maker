10ad4aa62a7bac3d307a99fbf832bf31d7a8d0ff
Moomi98 <kymkjh2002@gmail.com>
2023-02-16 19:28:38 +0900

    style : Global font 적용

--- styles/global.ts
+++ styles/global.ts
@@ -1,9 +1,47 @@
 import { createGlobalStyle } from "styled-components";
+import NotoSansKR_Black from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Black.otf";
+import NotoSansKR_Bold from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Bold.otf";
+import NotoSansKR_Light from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Light.otf";
+import NotoSansKR_Medium from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Medium.otf";
+import NotoSansKR_Regular from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Regular.otf";
+import NotoSansKR_Thin from "@/public/fonts/Noto_Sans_KR/NotoSansKR-Thin.otf";
 
 export const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "NotoSansKR-Black";
+    font-style: normal;
+    src: url(${NotoSansKR_Black});
+  }
+  @font-face {
+    font-family: "NotoSansKR-Bold";
+    font-style: normal;
+    src: url(${NotoSansKR_Bold});
+  }
+  @font-face {
+    font-family: "NotoSansKR-Light";
+    font-style: normal;
+    src: url(${NotoSansKR_Light});
+  }
+  @font-face {
+    font-family: "NotoSansKR-Medium";
+    font-style: normal;
+    src: url(${NotoSansKR_Medium});
+  }
+  @font-face {
+    font-family: "NotoSansKR-Regular";
+    font-style: normal;
+    src: url(${NotoSansKR_Regular});
+  }
+  @font-face {
+    font-family: "NotoSansKR-Thin";
+    font-style: normal;
+    src: url(${NotoSansKR_Thin});
+  }
 *{
     box-sizing: border-box;
-}
+    font-family: "NotoSansKR-Regular";
+}ß
   html, body {
     height: 100%;
     padding: 0;
